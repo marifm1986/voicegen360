3d7c88db39a980c330b1bcc8d5d546420fd84723
marifm1986 <marifm1986@gmail.com>
2026-06-17 22:43:10 +0600

    s

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import { createServer as createViteServer } from "vite";
 import { GoogleGenAI, Type } from "@google/genai";
 import dotenv from "dotenv";
 
+dotenv.config({ path: ".env.local" });
 dotenv.config();
 
 const app = express();
